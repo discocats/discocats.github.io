7269aacfaf3be7bd292e65f37202568bc525de57
Duscicat <duscicat@gmail.com>
2019-02-01 02:37:25 +0700

    main page

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var url = window.location;
+$('ul.navbar-nav a[href="'+ url +'"]').parent().addClass(" " + 'active');
+$('ul.navbar-nav a').filter(function() {
+    return this.href == url;
+}).parent().addClass(" " + 'active');

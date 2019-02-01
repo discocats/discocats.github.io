d48da30934c8c001bf8f895992511a89c6320edc
Duscicat <duscicat@gmail.com>
2019-02-01 17:08:31 +0700

    add company.html

--- js/main.js
+++ js/main.js
@@ -3,3 +3,20 @@ $('ul.navbar-nav a[href="'+ url +'"]').parent().addClass(" " + 'active');
 $('ul.navbar-nav a').filter(function() {
     return this.href == url;
 }).parent().addClass(" " + 'active');
+
+$('ul.navbar-nav li.nav-item a[href="'+ url +'"]').css("color", "#FFBB3F");
+$('ul.navbar-nav li.nav-item a').filter(function() {
+    return this.href == url;
+}).css("color", "#FFBB3F");
+
+$('[data-fancybox="mainPicture"]').fancybox({
+  protect    : true,
+  slideClass : 'mainPicture',
+  toolbar    : false,
+  smallBtn   : true,
+  buttons : [
+    'zoom',
+    'thumbs',
+    'close'
+  ]
+});

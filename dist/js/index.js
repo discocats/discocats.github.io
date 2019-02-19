4fa59cb59908a5d8189d1b57b46d17c7c34b067c
Duscicat <duscicat@gmail.com>
2019-02-19 19:06:44 +0700

    #66 add new products

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,6 +1,21 @@
 const sweets = [
     { imageBigUrl: "dist/img/tetes_brulees_blueberry_Big.png", imageUrl: "dist/img/tetes_brulees_blueberry.png", name: "Tetes Brulees Blue Raspberry Color", description: "Кислая малина", price: 42 },
     { imageBigUrl: "dist/img/top_pops_berry_Big.png", imageUrl: "dist/img/top_pops_berry.png", name: "Top Pops Red and Blue Raspberry", description: "Вкусные ретро жевательные ириски", price: 54 },
+    { imageBigUrl: "dist/img/top_pops_watermelon_Big.png", imageUrl: "dist/img/top_pops_watermelon.png", name: "Top Pops Watermelon", description: "Вкусные ретро жевательные ириски", price: 33},
+    { imageBigUrl: "dist/img/tetes_brulees_blueberry_Big.png", imageUrl: "dist/img/tetes_brulees_blueberry.png", name: "Tetes Brulees Blue Raspberry Color", description: "Кислая малина", price: 42 },
+    { imageBigUrl: "dist/img/top_pops_berry_Big.png", imageUrl: "dist/img/top_pops_berry.png", name: "Top Pops Red and Blue Raspberry", description: "Вкусные ретро жевательные ириски", price: 54 },
+    { imageBigUrl: "dist/img/top_pops_watermelon_Big.png", imageUrl: "dist/img/top_pops_watermelon.png", name: "Top Pops Watermelon", description: "Вкусные ретро жевательные ириски", price: 33},
+    { imageBigUrl: "dist/img/tetes_brulees_blueberry_Big.png", imageUrl: "dist/img/tetes_brulees_blueberry.png", name: "Tetes Brulees Blue Raspberry Color", description: "Кислая малина", price: 42 },
+    { imageBigUrl: "dist/img/top_pops_berry_Big.png", imageUrl: "dist/img/top_pops_berry.png", name: "Top Pops Red and Blue Raspberry", description: "Вкусные ретро жевательные ириски", price: 54 },
+    { imageBigUrl: "dist/img/top_pops_watermelon_Big.png", imageUrl: "dist/img/top_pops_watermelon.png", name: "Top Pops Watermelon", description: "Вкусные ретро жевательные ириски", price: 33},
+    { imageBigUrl: "dist/img/tetes_brulees_blueberry_Big.png", imageUrl: "dist/img/tetes_brulees_blueberry.png", name: "Tetes Brulees Blue Raspberry Color", description: "Кислая малина", price: 42 },
+    { imageBigUrl: "dist/img/top_pops_berry_Big.png", imageUrl: "dist/img/top_pops_berry.png", name: "Top Pops Red and Blue Raspberry", description: "Вкусные ретро жевательные ириски", price: 54 },
+    { imageBigUrl: "dist/img/top_pops_watermelon_Big.png", imageUrl: "dist/img/top_pops_watermelon.png", name: "Top Pops Watermelon", description: "Вкусные ретро жевательные ириски", price: 33},
+    { imageBigUrl: "dist/img/tetes_brulees_blueberry_Big.png", imageUrl: "dist/img/tetes_brulees_blueberry.png", name: "Tetes Brulees Blue Raspberry Color", description: "Кислая малина", price: 42 },
+    { imageBigUrl: "dist/img/top_pops_berry_Big.png", imageUrl: "dist/img/top_pops_berry.png", name: "Top Pops Red and Blue Raspberry", description: "Вкусные ретро жевательные ириски", price: 54 },
+    { imageBigUrl: "dist/img/top_pops_watermelon_Big.png", imageUrl: "dist/img/top_pops_watermelon.png", name: "Top Pops Watermelon", description: "Вкусные ретро жевательные ириски", price: 33},
+    { imageBigUrl: "dist/img/tetes_brulees_blueberry_Big.png", imageUrl: "dist/img/tetes_brulees_blueberry.png", name: "Tetes Brulees Blue Raspberry Color", description: "Кислая малина", price: 42 },
+    { imageBigUrl: "dist/img/top_pops_berry_Big.png", imageUrl: "dist/img/top_pops_berry.png", name: "Top Pops Red and Blue Raspberry", description: "Вкусные ретро жевательные ириски", price: 54 },
     { imageBigUrl: "dist/img/top_pops_watermelon_Big.png", imageUrl: "dist/img/top_pops_watermelon.png", name: "Top Pops Watermelon", description: "Вкусные ретро жевательные ириски", price: 33}
 ];
 
@@ -8,7 +23,7 @@ function fromSweetToArticle(data) {
     return "<article class='col-12 col-md-4'>"
     + `<a href=${data.imageBigUrl} data-fancybox="sweetPicture"> <img src=${data.imageUrl} class='rounded-circle' alt='Фотография товара'/> </a>`
     + `<h4>${data.name}</h4>`
-    + `<p>${data.description}</p>`
+    + `<p id="descrProduct">${data.description}</p>`
     + `<p id="price">${data.price} &#8381;</p>` + '<p id="addCart" onclick="addProductToCart()"> <i class="fas fa-cart-plus"></i> </p>'
     + "</article>";
 }

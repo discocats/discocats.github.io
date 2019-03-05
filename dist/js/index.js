f11d35728497086befd049af20c21b6a12bbddc6
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2019-03-05 10:35:40 +0300

    advanced babel dev

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,19 +1,21 @@
 "use strict";
 
-let clicks = 0;
+require("core-js/modules/es6.function.name");
+
+var clicks = 0;
 
 function addProductToCart() {
   document.getElementById("clicks").innerHTML = ++clicks;
 }
 
 function fromSweetToArticle(sweet) {
-  return "<article class='col-12 col-md-4'>" + `<a href=${sweet.imageBigUrl} data-fancybox="sweetPicture"> <img src=${sweet.imageUrl} class='rounded-circle' alt='Фотография товара'/> </a>` + `<h4>${sweet.name}</h4>` + `<p id="descrProduct">${sweet.description}</p>` + `<p id="price">${sweet.price} &#8381;</p>` + '<p id="addCart" onclick="addProductToCart()"> <i class="fas fa-cart-plus"></i> </p>' + "</article>";
+  return "<article class='col-12 col-md-4'>" + "<a href=".concat(sweet.imageBigUrl, " data-fancybox=\"sweetPicture\"> <img src=").concat(sweet.imageUrl, " class='rounded-circle' alt='\u0424\u043E\u0442\u043E\u0433\u0440\u0430\u0444\u0438\u044F \u0442\u043E\u0432\u0430\u0440\u0430'/> </a>") + "<h4>".concat(sweet.name, "</h4>") + "<p id=\"descrProduct\">".concat(sweet.description, "</p>") + "<p id=\"price\">".concat(sweet.price, " &#8381;</p>") + '<p id="addCart" onclick="addProductToCart()"> <i class="fas fa-cart-plus"></i> </p>' + "</article>";
 }
 
 function fromSweetsToHtml(sweets) {
-  let finalHtml = "";
+  var finalHtml = "";
 
-  for (let i = 0; i < sweets.length; i++) {
+  for (var i = 0; i < sweets.length; i++) {
     finalHtml += fromSweetToArticle(sweets[i]);
   }
 
@@ -21,12 +23,16 @@ function fromSweetsToHtml(sweets) {
 }
 
 function updateCatalog(sweets) {
-  const html = fromSweetsToHtml(sweets);
-  const catalog = document.getElementsByClassName("catalog")[0];
+  var html = fromSweetsToHtml(sweets);
+  var catalog = document.getElementsByClassName("catalog")[0];
   catalog.innerHTML = html;
 }
 
-fetch('https://whispering-sea-97013.herokuapp.com/products').then(response => response.json()).then(sweets => updateCatalog(sweets)).catch(error => {
+fetch('https://whispering-sea-97013.herokuapp.com/products').then(function (response) {
+  return response.json();
+}).then(function (sweets) {
+  return updateCatalog(sweets);
+}).catch(function (error) {
   alert(error);
 });
 var formLogin = new FormData(document.getElementById('loginForm'));

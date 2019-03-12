00e7656a5a4c3e3bca208b960bff0b3d15f11231
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2019-03-12 11:35:23 +0300

    #94 ts configured, gitignore updated, logging rewritten

---
+++ src/build/logging.ts
@@ -0,0 +1,22 @@
+import Colors = require("colors");
+
+export function logSuccess(message: any) {
+    if (message) {
+        // tslint:disable-next-line:no-console
+        console.log(message.green);
+    }
+}
+
+export function logError(message: any) {
+    if (message) {
+        // tslint:disable-next-line:no-console
+        console.log(message.red);
+    }
+}
+
+export function logInfo(message: any) {
+    if (message) {
+        // tslint:disable-next-line:no-console
+        console.log(message.blue);
+    }
+}

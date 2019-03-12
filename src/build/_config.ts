d17d6c7b84cacbf14ada4be2cd7ce57ebb914e58
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2019-03-12 11:54:54 +0300

    #94 dev progress

---
+++ src/build/_config.ts
@@ -0,0 +1,17 @@
+export const config = {
+    html: {
+        outFilePath: "./index.html",
+    },
+    scripts: {
+        inDirPath: "./src/scripts",
+        inFilePath: "./src/scripts/index.tsx",
+        outDirPath: "./dist/js",
+        outFileName: "index.js",
+        outFilePath: "./dist/js/index.js",
+    },
+    styles: {
+        allSrcFiles: "./src/styles/**/*.scss",
+        inFilePath: "./src/styles/index.scss",
+        outFilePath: "./dist/styles/index.css",
+    },
+};
---
+++ src/build/_tslint.ts
@@ -0,0 +1,50 @@
+import fs from "fs-extra";
+import glob from "glob";
+import tslint from "tslint";
+
+import { config } from "./_config";
+const {
+    scripts: { inDirPath },
+} = config;
+import * as log from "./logging";
+
+function lintTypescript() {
+    const options: tslint.ILinterOptions = {
+        fix: false,
+        formatter: "stylish",
+        formattersDirectory: null,
+        rulesDirectory: null,
+    };
+
+    const linter = new tslint.Linter(options);
+
+    const filePaths = glob.sync(inDirPath + "/**/*.ts?(x)");
+
+    const filesWithErrors = [];
+    filePaths.forEach((path) => {
+        const fileContents = fs.readFileSync(path, "utf8");
+
+        const configuration = tslint.Configuration.findConfiguration("./tslint.json", path).results;
+
+        linter.lint(path, fileContents, configuration);
+        const result = linter.getResult();
+
+        if (result.errorCount > 0) {
+            filesWithErrors.push({
+                errorsCount: result.errorCount,
+                path,
+            });
+
+            log.logError(result.output);
+        }
+
+        (linter as any).failures = [];
+        (linter as any).fixes = [];
+    });
+
+    if (filesWithErrors.length === 0) {
+        log.logSuccess("Typescript linter check found no errors!");
+    }
+}
+
+module.exports = { lintTypescript };
